82f42f9a088d94a5b472695ae3f9440a1b20d7ae
Cesar Roman <cesardroman18@gmail.com>
2023-03-27 10:32:31 -0500

    implements time travel

--- client/App.jsx
+++ client/App.jsx
@@ -3,12 +3,31 @@ import React, { useState, useEffect } from 'react';
 const Game = () => {
   const [isXNext, setIsXNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setIsXNext(!isXNext);
   };
+
+  const jumptTo = (nextMove) => {
+    setCurrentMove(nextMove);
+    setIsXNext(nextMove % 2 === 0);
+  };
+
+  const moves = history.map((squares, move) => {
+    const description = move > 0 ? `Go to move #${move}` : 'Go to game start';
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumptTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
@@ -17,7 +36,7 @@ const Game = () => {
       </div>
 
       <div className="game-info">
-        <ol></ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
